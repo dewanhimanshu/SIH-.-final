37f12e4a05bbc4c689dfc7fe026c5afbd90ac71d
SHLOK WALIA <virgoshlok@gmail.com>
2018-03-27 12:34:50 +0530

    bugs resolved

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326152337) do
-
+ActiveRecord::Schema.define(version: 20180326112023) do
 
   create_table "answers", force: :cascade do |t|
     t.text "content"
