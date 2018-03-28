cc0919b5ac0e73b224e4c412c3e595dc10862d0a
yashgupta <ygroks1998@gmail.com>
2018-03-29 03:00:02 +0530

    home page and some features edited

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
-  def index
-render :layout => false
+def index
+  if user_signed_in?
+    @user=current_user
   end
+render :layout => false
+end
 
   def delete
     redirect_to destroy_user_session_path
